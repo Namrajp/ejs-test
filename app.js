ce8aaab3c782f0b3ac887badfdc9290954e0194a
Namraj <active99raj@gmail.com>
2025-09-13 18:59:23 +1000

    Added: form and handled in app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 // Set EJS as the view engine
 app.set("view engine", "ejs");
+// Parse URL-encoded form data
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   //   const data = {
@@ -18,6 +21,17 @@ app.get("/", (req, res) => {
   //   ];
   // Render ejs template and pass data
   res.render("index", { username: "Amar Pudasaini", items: fruits });
+});
+
+// Handle POST request when the form is submitted
+app.post("/submit", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+
+  // Process the form data (e.g., save to a database)
+
+  // Redirect or render a response
+  res.send(`Thank you, ${name}, for submitting your email (${email}).`);
 });
 
 app.listen(3000, () => {
