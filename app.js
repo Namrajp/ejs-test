33a63e39e43fd448f6ae52ff217602efd738271b
Namraj <active99raj@gmail.com>
2025-09-13 18:48:00 +1000

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+// Set EJS as the view engine
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  //   const data = {
+  //     username: "Amar Pudasaini",
+  //   };
+
+  const fruits = ["Apple", "Banana", "Orange"];
+  //   const items = [
+  //     { name: "electronics", quantity: 5 },
+  //     { name: "furniture", quantity: 6 },
+  //     { name: "cloths", quantity: 43 },
+  //     { name: "books", quantity: 4 },
+  //   ];
+  // Render ejs template and pass data
+  res.render("index", { username: "Amar Pudasaini", items: fruits });
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
